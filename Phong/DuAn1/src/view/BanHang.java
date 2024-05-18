d29bc95beda6c08116c9d33ccd264a6f381c947b
trunganhishere <duongtrunganh204@gmail.com>
2024-05-19 00:10:35 +0700

    upload ban hang

--- Phong/DuAn1/src/view/BanHang.java
+++ Phong/DuAn1/src/view/BanHang.java
@@ -3,8 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JPanel.java to edit this template
  */
 package view;
-// phong an cut
-//hehe
+
 import model.HoaDonChiTiet;
 import Interface.hoaDonInterface;
 import java.text.DecimalFormat;
