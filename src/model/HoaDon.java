00d175946372a091abeff73aaa953f7073d3471c
trunganhishere <duongtrunganh204@gmail.com>
2024-05-18 17:08:23 +0700

    upload

---
+++ src/model/HoaDon.java
@@ -0,0 +1,123 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+import java.util.Date;
+
+/**
+ *
+ * @author duong
+ */
+public class HoaDon {
+    private Integer id;
+    private Integer idKH;
+    private Integer idNV;
+    private String ma;
+    private Date ngayThanhToan;
+    private Boolean tinhTrang;
+    private String ghiChu;
+    private Integer tongTien;
+    private Date ngayTao;
+    private Date ngaySua;
+
+    public HoaDon() {
+    }
+
+    public HoaDon(Integer id, Integer idKH, Integer idNV, String ma, Date ngayThanhToan, Boolean tinhTrang, String ghiChu, Integer tongTien, Date ngayTao, Date ngaySua) {
+        this.id = id;
+        this.idKH = idKH;
+        this.idNV = idNV;
+        this.ma = ma;
+        this.ngayThanhToan = ngayThanhToan;
+        this.tinhTrang = tinhTrang;
+        this.ghiChu = ghiChu;
+        this.tongTien = tongTien;
+        this.ngayTao = ngayTao;
+        this.ngaySua = ngaySua;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getIdKH() {
+        return idKH;
+    }
+
+    public void setIdKH(Integer idKH) {
+        this.idKH = idKH;
+    }
+
+    public Integer getIdNV() {
+        return idNV;
+    }
+
+    public void setIdNV(Integer idNV) {
+        this.idNV = idNV;
+    }
+
+    public String getMa() {
+        return ma;
+    }
+
+    public void setMa(String ma) {
+        this.ma = ma;
+    }
+
+    public Date getNgayThanhToan() {
+        return ngayThanhToan;
+    }
+
+    public void setNgayThanhToan(Date ngayThanhToan) {
+        this.ngayThanhToan = ngayThanhToan;
+    }
+
+    public Boolean getTinhTrang() {
+        return tinhTrang;
+    }
+
+    public void setTinhTrang(Boolean tinhTrang) {
+        this.tinhTrang = tinhTrang;
+    }
+
+    public String getGhiChu() {
+        return ghiChu;
+    }
+
+    public void setGhiChu(String ghiChu) {
+        this.ghiChu = ghiChu;
+    }
+
+    public Integer getTongTien() {
+        return tongTien;
+    }
+
+    public void setTongTien(Integer tongTien) {
+        this.tongTien = tongTien;
+    }
+
+    public Date getNgayTao() {
+        return ngayTao;
+    }
+
+    public void setNgayTao(Date ngayTao) {
+        this.ngayTao = ngayTao;
+    }
+
+    public Date getNgaySua() {
+        return ngaySua;
+    }
+
+    public void setNgaySua(Date ngaySua) {
+        this.ngaySua = ngaySua;
+    }
+
+    
+    
+}
