b03e502a24b476d8e58c4a8d9d48a7874dd82889
trunganhishere <duongtrunganh204@gmail.com>
2024-05-18 17:08:23 +0700

    upload

---
+++ src/model/NhaSX.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author duong
+ */
+public class NhaSX {
+    private int id;
+    private String ten;
+
+    public NhaSX() {
+    }
+
+    public NhaSX(int id, String ten) {
+        this.id = id;
+        this.ten = ten;
+    }
+
+    public String getTen() {
+        return ten;
+    }
+
+    public void setTen(String ten) {
+        this.ten = ten;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+    
+
+    @Override
+    public String toString() {
+        return ten;
+    }
+}
